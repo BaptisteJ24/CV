531fd1324b0136ccf7aedd9ad72767245a4b498c
Baptiste JAN <baptistejan@outlook.fr>
2022-08-23 11:22:22 +0200

    Premier Push
    Premier Push

---
+++ assets/js/menu.js
@@ -0,0 +1,25 @@
+import("https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js")
+var menu_container = document.getElementById("menu");
+var liens_menus = {
+    "index.html": "Accueil",
+    "skills.html": "Compétences",
+    "experience.html": "Formations et Expériences Professionnelles",
+    "projects.html": "Projets",
+    "propos.html": "A Propos",
+};
+
+menu_container.innerHTML +='<nav class="menu" id="page__menu" name="page__menu"><ul class="menu__list r-list">';
+
+for(var i in liens_menus){
+    let menu = document.createElement('a');
+    menu.setAttribute('href', i)
+    menu.setAttribute('class', "menu__link r-link text-underlined")
+    menu.innerHTML = liens_menus[i]
+    document.getElementById('page__menu').append(menu)
+}
+
+
+
+
+		
+	
