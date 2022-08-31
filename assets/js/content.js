8aa26ce35c0f5dd84fa389f1f860520f27afd44f
Baptiste JAN <baptistejan@outlook.fr>
2022-08-31 18:43:22 +0200

    mise en page du code

--- assets/js/content.js
+++ assets/js/content.js
@@ -16,10 +16,56 @@ class formationClass {
     }
 }
 
-let openClassrooms = new formationClass("Développeur d'application JavaScript React", 2022, "present", "OpenClassrooms", "Créer des sites web statiques en HTML/CSS et dynamiques en JavaScript React. Utilisation d’API. Mise en oeuvre de tests unitaires et débogage du code. Résolutions de problèmes et algorithmie. Modernisation et optimisation du code.", "RNCP, diplôme de niveau 6 (bac+3/+4)", "Informatique", "", "", "logo_Laptop");
-let licencePsy = new formationClass("Licence de Science Humaine et Sociale", 2018, 2022, "Université d'Angers, UFR LLSH, Angers", "", "", "Psychologie", "", "assez bien", "logo_Psychology");
-let btsSIO = new formationClass("BTS Services Informatiques aux Organisations, Solutions Logicielles et Applications Métiers", 2016, 2018, "Lycée Chevrollier, Angers", "", "", "Informatique", "mathéématiques approfondies", "", "logo_Laptop");
-let bacES = new formationClass("Baccalauréat Économique et Social", 2014, 2016, "Lycée Saint-Michel, Château-Gontier", "", "", "Économie et social", "mathématiques", "", "logo_ses");
+let openClassrooms = new formationClass(
+    "Développeur d'application JavaScript React",
+    2022,
+    "present",
+    "OpenClassrooms",
+    "Créer des sites web statiques en HTML/CSS et dynamiques en JavaScript React. Utilisation d’API. Mise en oeuvre de tests unitaires et débogage du code. Résolutions de problèmes et algorithmie. Modernisation et optimisation du code.",
+    "RNCP, diplôme de niveau 6 (bac+3/+4)",
+    "Informatique",
+    "",
+    "",
+    "logo_Laptop"
+);
+
+let licencePsy = new formationClass(
+    "Licence de Science Humaine et Sociale",
+    2018,
+    2022,
+    "Université d'Angers, UFR LLSH, Angers",
+    "",
+    "",
+    "Psychologie",
+    "",
+    "assez bien",
+    "logo_Psychology"
+);
+
+let btsSIO = new formationClass(
+    "BTS Services Informatiques aux Organisations, Solutions Logicielles et Applications Métiers",
+    2016,
+    2018,
+    "Lycée Chevrollier, Angers",
+    "",
+    "",
+    "Informatique",
+    "mathéématiques approfondies",
+    "",
+    "logo_Laptop"
+);
+
+let bacES = new formationClass(
+    "Baccalauréat Économique et Social",
+    2014, 2016,
+    "Lycée Saint-Michel, Château-Gontier",
+    "",
+    "",
+    "Économie et social",
+    "mathématiques",
+    "",
+    "logo_ses"
+);
 
 
 let formationTable = [openClassrooms, licencePsy, btsSIO, bacES];
@@ -88,7 +134,6 @@ function LoadFormationContent() {
 LoadFormationContent();
 
 
-
 class experienceProClass {
 
     constructor(name, startDate, endDate, lieu, description, theme, image, dev, fonction) {
@@ -105,12 +150,76 @@ class experienceProClass {
 }
 
 
-let Octave = new experienceProClass("Stagiaire en développement web", "01/2018", "03/2018", "Octave, Angers", "Refonte de la partie administrations des sites internet de l'entreprise. Utilisation des langages HTML, CSS, PHP, AngularJS et SQL. Méthode AGILE", "Informatique", "logo_Laptop", true, "Stagiaire");
-let stMichel = new experienceProClass("Stagiaire en développement web", "01/2017", "02/2017", "Lycée Saint-Michel, Château-Gontier-sur-Mayenne", "Développement d'une application web destinée à l'administration du lycée. Utilisation des langages HTML, CSS, PHP et SQL", "Informatique", "logo_Laptop", true, "Stagiaire");
-let PsyEN = new experienceProClass("Stagiaire auprès d'un Psychologue de l'Éducation Nationale", "01/2022", "03/2022", "CIO Mayenne Sud, Laval", "Observation et participation à des entretiens individuels. Cotation de tests psychométriques", "Sciences Humaines et Sociales, Psychologie", "logo_Psychology", false, "Stagiaire");
-let MLAC = new experienceProClass("Médiateur de Lutte Anti-Covid", "08/2021", "12/2021", "Protection Civile du Maine et Loire, Sainte-Gemme-Sur-Loire", "Tests antigéniques tout public. Accueil administratif, prélèvements, analyse de tests, rendu de résultats. Accompagnement, prévention et sensibilisation.", "Santé", "logo_MLAC", false, "Intérimaire");
-let agentTechFL = new experienceProClass("Agent Technique, Service Fête et Logistique", "07/2019", "08/2019", "Communauté de Communes du Pays de Château-Gontier, Château-Gontier-Sur-Mayenne", "Manutention, Préparation des installations pour des événements (gradins, barnums, tentes). Montage et démontage du matériel lors des manifestations", "Logistique et évènementiel", "logo_Handling", false, "Saisonnier");
-let agentCollDechet = new experienceProClass("Agent de collecte des ordures ménagères", "07/2018", "08/2018", "Communauté de Communes du Pays de Château-Gontier, Château-Gontier sur Mayenne", "Collecte des ordures ménagères", "Environnement, Hygiène", "logo_BinLorry", false, "Saisonnier");
+let Octave = new experienceProClass(
+    "Stagiaire en développement web", 
+    "01/2018", 
+    "03/2018", 
+    "Octave, Angers", 
+    "Refonte de la partie administrations des sites internet de l'entreprise. Utilisation des langages HTML, CSS, PHP, AngularJS et SQL. Méthode AGILE", 
+    "Informatique", 
+    "logo_Laptop", 
+    true, 
+    "Stagiaire"
+);
+
+let stMichel = new experienceProClass(
+    "Stagiaire en développement web",
+    "01/2017",
+    "02/2017",
+    "Lycée Saint-Michel, Château-Gontier-sur-Mayenne",
+    "Développement d'une application web destinée à l'administration du lycée. Utilisation des langages HTML, CSS, PHP et SQL", 
+    "Informatique", 
+    "logo_Laptop", true, 
+    "Stagiaire"
+);
+
+let PsyEN = new experienceProClass(
+    "Stagiaire auprès d'un Psychologue de l'Éducation Nationale", 
+    "01/2022", 
+    "03/2022", 
+    "CIO Mayenne Sud, Laval", 
+    "Observation et participation à des entretiens individuels. Cotation de tests psychométriques", 
+    "Sciences Humaines et Sociales, Psychologie", 
+    "logo_Psychology", 
+    false, 
+    "Stagiaire"
+);
+
+let MLAC = new experienceProClass(
+    "Médiateur de Lutte Anti-Covid", 
+    "08/2021", 
+    "12/2021", 
+    "Protection Civile du Maine et Loire, Sainte-Gemme-Sur-Loire", 
+    "Tests antigéniques tout public. Accueil administratif, prélèvements, analyse de tests, rendu de résultats. Accompagnement, prévention et sensibilisation.", 
+    "Santé", 
+    "logo_MLAC", 
+    false, 
+    "Intérimaire"
+);
+
+let agentTechFL = new experienceProClass(
+    "Agent Technique, Service Fête et Logistique", 
+    "07/2019", 
+    "08/2019", 
+    "Communauté de Communes du Pays de Château-Gontier, Château-Gontier-Sur-Mayenne", 
+    "Manutention, Préparation des installations pour des événements (gradins, barnums, tentes). Montage et démontage du matériel lors des manifestations", 
+    "Logistique et évènementiel", 
+    "logo_Handling", 
+    false, 
+    "Saisonnier"
+);
+
+let agentCollDechet = new experienceProClass(
+    "Agent de collecte des ordures ménagères", 
+    "07/2018", 
+    "08/2018", 
+    "Communauté de Communes du Pays de Château-Gontier, Château-Gontier sur Mayenne", 
+    "Collecte des ordures ménagères", 
+    "Environnement, Hygiène", 
+    "logo_BinLorry", 
+    false, 
+    "Saisonnier"
+);
 
 
 let experienceProTable = [PsyEN, MLAC, agentTechFL, agentCollDechet, Octave, stMichel];
