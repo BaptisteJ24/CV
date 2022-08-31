2e8d2b333f61f104c3e4545803e545dcb0ac68b4
Baptiste JAN <baptistejan@outlook.fr>
2022-08-31 20:57:00 +0200

    Update experiencePro

--- assets/js/content.js
+++ assets/js/content.js
@@ -2,30 +2,30 @@ import("https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js");
 
 class formationClass {
 
-    constructor(name, startDate, endDate, lieu, description, othersDetails, theme, option, mention, image) {
+    constructor(name, startDate, endDate, place, description, othersDetails, theme, option, mention, images) {
         this.name = name;
         this.startDate = startDate;
         this.endDate = endDate;
-        this.lieu = lieu;
+        this.place = place;
         this.description = description;
         this.othersDetails = othersDetails;
         this.theme = theme;
         this.option = option;
         this.mention = mention;
-        this.image = image;
+        this.images = images;
     }
 }
 
 let openClassrooms = new formationClass(
-    "Développeur d'application JavaScript React",
+    "Formation Développeur d'application JavaScript React",
     2022,
-    "present",
+    "présent",
     "OpenClassrooms",
     "Créer des sites web statiques en HTML/CSS et dynamiques en JavaScript React. Utilisation d’API. Mise en oeuvre de tests unitaires et débogage du code. Résolutions de problèmes et algorithmie. Modernisation et optimisation du code.",
     "RNCP, diplôme de niveau 6 (bac+3/+4)",
     "Informatique",
-    "",
-    "",
+    null,
+    null,
     "logo_Laptop"
 );
 
@@ -34,10 +34,10 @@ let licencePsy = new formationClass(
     2018,
     2022,
     "Université d'Angers, UFR LLSH, Angers",
-    "",
-    "",
+    null,
+    null,
     "Psychologie",
-    "",
+    null,
     "assez bien",
     "logo_Psychology"
 );
@@ -47,11 +47,11 @@ let btsSIO = new formationClass(
     2016,
     2018,
     "Lycée Chevrollier, Angers",
-    "",
-    "",
+    null,
+    null,
     "Informatique",
-    "mathéématiques approfondies",
-    "",
+    "mathématiques approfondies",
+    null,
     "logo_Laptop"
 );
 
@@ -59,11 +59,11 @@ let bacES = new formationClass(
     "Baccalauréat Économique et Social",
     2014, 2016,
     "Lycée Saint-Michel, Château-Gontier",
-    "",
-    "",
+    null,
+    null,
     "Économie et social",
     "mathématiques",
-    "",
+    null,
     "logo_ses"
 );
 
@@ -73,6 +73,7 @@ let formationTable = [openClassrooms, licencePsy, btsSIO, bacES];
 function LoadFormationContent() {
     var i = 0;
     for (formationClass in formationTable) {
+
         let divFormation = document.createElement('div');
         divFormation.setAttribute('class', 'div_Experience_Container');
         divFormation.setAttribute('id', 'divFormation' + i);
@@ -81,9 +82,9 @@ function LoadFormationContent() {
         divDate.setAttribute('class', 'div_Date');
         divDate.setAttribute('id', 'div_Date_Formation' + i);
 
-        let divImageText = document.createElement('div');
-        divImageText.setAttribute('class', 'div_Image_Text');
-        divImageText.setAttribute('id', 'div_ImageText_Formation' + i);
+        let divDateText = document.createElement('div');
+        divDateText.setAttribute('class', 'div_Date_Text');
+        divDateText.setAttribute('id', 'div_DateText_Formation' + i);
 
         let divImages = document.createElement('div');
         divImages.setAttribute('class', 'div_Images');
@@ -93,40 +94,48 @@ function LoadFormationContent() {
         divText.setAttribute('class', 'div_Text');
         divText.setAttribute('id', 'div_Text_Formation' + i);
 
-        /* Affiche la date de la Formation */
         let formationDate = document.createElement('h3');
         formationDate.innerHTML += formationTable[formationClass].startDate + "-" + formationTable[formationClass].endDate;
-        /*Affiche le nom de la formation*/
+
         let formationName = document.createElement('h2');
-        formationName.innerHTML += formationTable[formationClass].theme + ", " + formationTable[formationClass].name
-        /*Affiche la description de la formation*/
+        formationName.innerHTML += formationTable[formationClass].theme + ", " + formationTable[formationClass].name;
+
+        if (formationTable[formationClass].mention !== null) {
+            formationName.innerHTML += ", mention " + formationTable[formationClass].mention;
+        }
+        if (formationTable[formationClass].option !== null) {
+            formationName.innerHTML += ", option " + formationTable[formationClass].option;
+        }
+
+        let formationPlace = document.createElement('h3');
+        formationPlace.setAttribute('class', 'place');
+        formationPlace.innerHTML += formationTable[formationClass].place;
+
+        if (formationTable[formationClass].othersDetails !== null) {
+            formationPlace.innerHTML += formationTable[formationClass].othersDetails;
+        }
+
         let formationDescription = document.createElement('h3');
-        formationDescription.innerHTML += formationTable[formationClass].description;
-        /* Affiche l'image de la formation*/
-        let formationImage = document.createElement('img')
-        formationImage.setAttribute('src', '../assets/png/' + formationTable[formationClass].image + '.png');
-        formationImage.setAttribute('class', 'image_Content');
-
-        /*On importe les données dans le document */
-
-        /*Création des lignes du tableau*/
-        document.getElementById('formationTable').append(divFormation);
-
-        /*Création des div date et ImageText dans des lignes*/
-        document.getElementById('divFormation' + i).append(divImages, divImageText);
+        if (formationTable[formationClass].description !== null) {
+            formationDescription.innerHTML += formationTable[formationClass].description;
+        }
 
 
-        /*Création des div Date et Text dans la div DateText*/
-        document.getElementById('div_ImageText_Formation' + i).append(divDate, divText);
+        let formationImages = document.createElement('img');
+        formationImages.setAttribute('src', '../assets/png/' + formationTable[formationClass].images + '.png');
+        formationImages.setAttribute('class', 'images_Content');
 
-        /*Création de l'élément date dans la div Date*/
-        document.getElementById('div_Date_Formation' + i).append(formationDate);
 
-        /*Création de l'élément image dans la div Image */
-        document.getElementById('div_Images_Formation' + i).append(formationImage);
+        function appendFormationContent() {
+            document.getElementById('formationTable').append(divFormation);
+            document.getElementById('divFormation' + i).append(divImages, divDateText);
+            document.getElementById('div_DateText_Formation' + i).append(divDate, divText);
+            document.getElementById('div_Date_Formation' + i).append(formationDate);
+            document.getElementById('div_Images_Formation' + i).append(formationImages);
+            document.getElementById('div_Text_Formation' + i).append(formationName, formationPlace, formationDescription);
+        }
 
-        /*Création de l'élément Text dans la div Text */
-        document.getElementById('div_Text_Formation' + i).append(formationName, formationDescription);
+        appendFormationContent();
         i++
     }
 }
@@ -136,14 +145,14 @@ LoadFormationContent();
 
 class experienceProClass {
 
-    constructor(name, startDate, endDate, lieu, description, theme, image, dev, fonction) {
+    constructor(name, startDate, endDate, place, description, theme, images, dev, fonction) {
         this.name = name;
         this.startDate = startDate;
         this.endDate = endDate;
-        this.lieu = lieu;
+        this.place = place;
         this.description = description;
         this.theme = theme;
-        this.image = image;
+        this.images = images;
         this.dev = dev;
         this.fonction = fonction;
     }
@@ -151,73 +160,73 @@ class experienceProClass {
 
 
 let Octave = new experienceProClass(
-    "Stagiaire en développement web", 
-    "01/2018", 
-    "03/2018", 
-    "Octave, Angers", 
-    "Refonte de la partie administrations des sites internet de l'entreprise. Utilisation des langages HTML, CSS, PHP, AngularJS et SQL. Méthode AGILE", 
-    "Informatique", 
-    "logo_Laptop", 
-    true, 
+    "Développeur Web",
+    "01/2018",
+    "03/2018",
+    "Octave, Angers",
+    "Refonte de la partie administrations des sites internet de l'entreprise. Utilisation des langages HTML, CSS, PHP, AngularJS et SQL. Méthode AGILE",
+    "Informatique",
+    "logo_Laptop",
+    true,
     "Stagiaire"
 );
 
 let stMichel = new experienceProClass(
-    "Stagiaire en développement web",
+    "Développeur Web",
     "01/2017",
     "02/2017",
     "Lycée Saint-Michel, Château-Gontier-sur-Mayenne",
-    "Développement d'une application web destinée à l'administration du lycée. Utilisation des langages HTML, CSS, PHP et SQL", 
-    "Informatique", 
-    "logo_Laptop", true, 
+    "Développement d'une application web destinée à l'administration du lycée. Utilisation des langages HTML, CSS, PHP et SQL",
+    "Informatique",
+    "logo_Laptop", true,
     "Stagiaire"
 );
 
 let PsyEN = new experienceProClass(
-    "Stagiaire auprès d'un Psychologue de l'Éducation Nationale", 
-    "01/2022", 
-    "03/2022", 
-    "CIO Mayenne Sud, Laval", 
-    "Observation et participation à des entretiens individuels. Cotation de tests psychométriques", 
-    "Sciences Humaines et Sociales, Psychologie", 
-    "logo_Psychology", 
-    false, 
+    "Psychologue de l'Éducation Nationale",
+    "01/2022",
+    "03/2022",
+    "CIO Mayenne Sud, Laval",
+    "Observation et participation à des entretiens individuels. Cotation de tests psychométriques",
+    "Psychologie",
+    "logo_Psychology",
+    false,
     "Stagiaire"
 );
 
 let MLAC = new experienceProClass(
-    "Médiateur de Lutte Anti-Covid", 
-    "08/2021", 
-    "12/2021", 
-    "Protection Civile du Maine et Loire, Sainte-Gemme-Sur-Loire", 
-    "Tests antigéniques tout public. Accueil administratif, prélèvements, analyse de tests, rendu de résultats. Accompagnement, prévention et sensibilisation.", 
-    "Santé", 
-    "logo_MLAC", 
-    false, 
+    "Médiateur de Lutte Anti-Covid",
+    "08/2021",
+    "12/2021",
+    "Protection Civile du Maine et Loire, Sainte-Gemme-Sur-Loire",
+    "Tests antigéniques tout public. Accueil administratif, prélèvements, analyse de tests, rendu de résultats. Accompagnement, prévention et sensibilisation.",
+    "Santé",
+    "logo_MLAC",
+    false,
     "Intérimaire"
 );
 
 let agentTechFL = new experienceProClass(
-    "Agent Technique, Service Fête et Logistique", 
-    "07/2019", 
-    "08/2019", 
-    "Communauté de Communes du Pays de Château-Gontier, Château-Gontier-Sur-Mayenne", 
-    "Manutention, Préparation des installations pour des événements (gradins, barnums, tentes). Montage et démontage du matériel lors des manifestations", 
-    "Logistique et évènementiel", 
-    "logo_Handling", 
-    false, 
+    "Agent Technique, Service Fête et Logistique",
+    "07/2019",
+    "08/2019",
+    "Communauté de Communes du Pays de Château-Gontier, Château-Gontier-Sur-Mayenne",
+    "Manutention, Préparation des installations pour des événements (gradins, barnums, tentes). Montage et démontage du matériel lors des manifestations",
+    "Logistique et évènementiel",
+    "logo_Handling",
+    false,
     "Saisonnier"
 );
 
 let agentCollDechet = new experienceProClass(
-    "Agent de collecte des ordures ménagères", 
-    "07/2018", 
-    "08/2018", 
-    "Communauté de Communes du Pays de Château-Gontier, Château-Gontier sur Mayenne", 
-    "Collecte des ordures ménagères", 
-    "Environnement, Hygiène", 
-    "logo_BinLorry", 
-    false, 
+    "Agent de collecte des ordures ménagères",
+    "07/2018",
+    "08/2018",
+    "Communauté de Communes du Pays de Château-Gontier, Château-Gontier sur Mayenne",
+    "Collecte des ordures ménagères",
+    "Environnement, Hygiène",
+    "logo_BinLorry",
+    false,
     "Saisonnier"
 );
 
@@ -235,9 +244,9 @@ function LoadExperienceProContent() {
         divDate.setAttribute('class', 'div_Date');
         divDate.setAttribute('id', 'div_Date_ExperiencePro' + i);
 
-        let divImageText = document.createElement('div');
-        divImageText.setAttribute('class', 'div_Image_Text');
-        divImageText.setAttribute('id', 'div_ImageText_ExperiencePro' + i);
+        let divDateText = document.createElement('div');
+        divDateText.setAttribute('class', 'div_Date_Text');
+        divDateText.setAttribute('id', 'div_DateText_ExperiencePro' + i);
 
         let divImages = document.createElement('div');
         divImages.setAttribute('class', 'div_Images');
@@ -247,39 +256,35 @@ function LoadExperienceProContent() {
         divText.setAttribute('class', 'div_Text');
         divText.setAttribute('id', 'div_Text_ExperiencePro' + i);
 
-        /* Affiche la date de la ExperiencePro */
+
         let experienceProDate = document.createElement('h3');
         experienceProDate.innerHTML += experienceProTable[experienceProClass].startDate + "- <br/>" + experienceProTable[experienceProClass].endDate;
-        /*Affiche le nom de la ExperiencePro*/
+
         let experienceProName = document.createElement('h2');
-        experienceProName.innerHTML += experienceProTable[experienceProClass].theme + ", " + experienceProTable[experienceProClass].name
-        /*Affiche la description de la ExperiencePro*/
+        experienceProName.innerHTML += experienceProTable[experienceProClass].theme + ", " + experienceProTable[experienceProClass].fonction + ", " + experienceProTable[experienceProClass].name
+
+        let experienceProPlace = document.createElement('h3');
+        experienceProPlace.setAttribute('class', 'place');
+        experienceProPlace.innerHTML += experienceProTable[experienceProClass].place;
+
         let experienceProDescription = document.createElement('h3');
         experienceProDescription.innerHTML += experienceProTable[experienceProClass].description;
-        /* Affiche l'image de la ExperiencePro*/
-        let experienceProImage = document.createElement('img')
-        experienceProImage.setAttribute('src', '../assets/png/' + experienceProTable[experienceProClass].image + '.png');
-        experienceProImage.setAttribute('class', 'image_Content');
 
-        /*On importe les données dans le document */
+        let experienceProImages = document.createElement('img')
+        experienceProImages.setAttribute('src', '../assets/png/' + experienceProTable[experienceProClass].images + '.png');
+        experienceProImages.setAttribute('class', 'images_Content');
 
-        /*Création des lignes du tableau*/
-        document.getElementById('experienceProTable').append(divExperiencePro);
 
-        /*Création des div date et ImageText dans des lignes*/
-        document.getElementById('divExperiencePro' + i).append(divImages, divImageText);
+        function appendExperienceContent() {
+            document.getElementById('experienceProTable').append(divExperiencePro);
+            document.getElementById('divExperiencePro' + i).append(divImages, divDateText);
+            document.getElementById('div_DateText_ExperiencePro' + i).append(divDate, divText);
+            document.getElementById('div_Date_ExperiencePro' + i).append(experienceProDate);
+            document.getElementById('div_Images_ExperiencePro' + i).append(experienceProImages);
+            document.getElementById('div_Text_ExperiencePro' + i).append(experienceProName, experienceProPlace, experienceProDescription);
+        }
 
-        /*Création des div Image et Text dans la div ImageText*/
-        document.getElementById('div_ImageText_ExperiencePro' + i).append(divDate, divText);
-
-        /*Création de l'élément date dans la div Date*/
-        document.getElementById('div_Date_ExperiencePro' + i).append(experienceProDate);
-
-        /*Création de l'élément image dans la div Image */
-        document.getElementById('div_Images_ExperiencePro' + i).append(experienceProImage);
-
-        /*Création de l'élément Text dans la div Text */
-        document.getElementById('div_Text_ExperiencePro' + i).append(experienceProName, experienceProDescription);
+        appendExperienceContent();
         i++
     }
 }
